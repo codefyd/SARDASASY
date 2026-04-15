8e7d8d724c78327d07584d64772c693e1de3a15f
codefyd <ahsnsy77@gmail.com>
2026-04-15 16:58:40 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: localStorage.getItem('sard_api_url') || 'https://script.google.com/macros/s/AKfycbxwBSf2SpKvI87PxgRO4eRHuD4a1WpcoAYz9dBajydUUsAmJMahm3gWoTiDJ87A/exec'
+  API_URL: localStorage.getItem('sard_api_url') || 'https://script.google.ciDJ87A/exec'
 };
 
 window.API_URL = window.APP_CONFIG.API_URL;
